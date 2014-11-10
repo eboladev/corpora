8a484b7a845e3aaab2aec5eb9c5e888aeef70c52
Poren Chiang <ren.chiang@gmail.com>
2014-11-10 22:43:02 +0800

    Trying to refactor dispatcher. Start from models

---
+++ server/messaging/models/model.py
@@ -0,0 +1,20 @@
+
+class Model(object):
+
+    def __init__(self, **kwargs):
+        pass
+
+    @classmethod
+    def get(cls, **kwargs):
+        pass
+
+    @classmethod
+    def all(cls):
+        pass
+
+    @classmethod
+    def filter(cls):
+        pass
+
+    def save(self):
+        pass
